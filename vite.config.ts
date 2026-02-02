cfce8434a8c040a9bba806eb8d7537f978b9cd84
AndreiNDP <andrei.neagu@toluna.com>
2026-02-02 18:02:49 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: Matches your GitHub repository name exactly
-  base: '/maxdiff_calculator_V2/', 
+  // Use relative base path so it works on any repo name
+  base: './', 
 });
